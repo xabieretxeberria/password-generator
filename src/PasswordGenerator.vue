4167afcb5df6a1abfe4ee1b1e00fad62e837043c
Xabi <xabier.etxeberria@orekainteractive.com>
2021-01-22 12:27:31 +0100

    Change the password length

--- src/PasswordGenerator.vue
+++ src/PasswordGenerator.vue
@@ -1,10 +1,19 @@
 <template>
-	
+	<div id="password-generator-app">
+		<form>
+			<label for="pass-length">Password length: </label>
+			<input id="pass-length" type="number" v-model="passwordLength" min="4" max="12" />
+		</form>
+	</div>
 </template>
 
 <script>
 export default {
-	
+	data() {
+		return {
+			passwordLength: 4
+		}
+	}
 }
 </script>
 

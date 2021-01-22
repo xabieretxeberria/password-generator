770fc7b97d7608204e6c5b2e3e841fd0112925fd
Xabi <xabier.etxeberria@orekainteractive.com>
2021-01-22 12:34:44 +0100

    Checkboxes for options

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Generate passwords based on certain characteristics selected by the user.
 ## User Stories
 
 - [x] User can select the length of the generated password
-- [ ] User can select one or multiple of the following: `Include uppercase letters`, `Include lowercase letters`, `Include numbers`, `Include symbols`
+- [x] User can select one or multiple of the following: `Include uppercase letters`, `Include lowercase letters`, `Include numbers`, `Include symbols`
 - [ ] By clicking the `Generate password` button, the user can see a password being generated
 - [ ] User can click a `Copy to clipboard` button which will save the password to the clipboard
 
--- src/PasswordGenerator.vue
+++ src/PasswordGenerator.vue
@@ -3,6 +3,18 @@
 		<form>
 			<label for="pass-length">Password length: </label>
 			<input id="pass-length" type="number" v-model="passwordLength" min="4" max="12" />
+			<br/>
+			<label for="pass-uppercase">Include uppercase letters: </label>
+			<input id="pass-uppercase" type="checkbox" v-model="passwordUppercase" />
+			<br/>
+			<label for="pass-lowercase">Include lowercase letters: </label>
+			<input id="pass-lowercase" type="checkbox" v-model="passwordLowercase" />
+			<br/>
+			<label for="pass-numbers">Include numbers: </label>
+			<input id="pass-numbers" type="checkbox" v-model="passwordNumbers" />
+			<br/>
+			<label for="pass-symbols">Include symbols: </label>
+			<input id="pass-symbols" type="checkbox" v-model="passwordSymbols" />
 		</form>
 	</div>
 </template>
@@ -11,7 +23,11 @@
 export default {
 	data() {
 		return {
-			passwordLength: 4
+			passwordLength: 4,
+			passwordUppercase: false,
+			passwordLowercase: false,
+			passwordNumbers: false,
+			passwordSymbols: false
 		}
 	}
 }
